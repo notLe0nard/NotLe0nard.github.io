ee472e55500aac5dc8f614316437c6672de2bd2f
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-01-09 19:29:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function openTwitch() {
   window.open('https://twitch.tv/NotLe0nard', '_blank');
 }
 function openODP() {
-  window.open('https://download1511.mediafire.com/vqysr2wt7wcgFJfibjFWRWXIlgVEkwswQyEaBtTIxR9gQLLSQPFUsF0xOZkyQLzpVg9TJmj8iD5D1LGaP1w7fya91trzcqAI8RrJC32-o7jsUwze_RF2CWemk-FGP6xbeTzlqUljUTTUZueSYIcIhwvaLXvva16mLO2V7sJOFhyl/isnmbwwc2urnl6n/WIDERSTANDSGRUPPE+%E2%80%9EDIE+WEI%C3%9FE+ROSE%E2%80%9D.odp', '_blank');
+  window.open('https://download1523.mediafire.com/gexlyqbey8sgZVdpIiOxsi12AJSEl6mDe-MjMhTwnZNp1fctLUcDyrUReptRc_-CGEET8--Q7HJCn0gz9xKb9r_CSeX6Ap4gVuhVUZZArbZR6AOSgdYPRFymTcdo2weK4cbG3WVvUpT7wiyBMEO5XY7B4dnW28e2B-vymAePd44p/gifc4nujgzpgxfz/WIDERSTANDSGRUPPE+%E2%80%9EDIE+WEI%C3%9FE+ROSE%E2%80%9D+%282%29.odp', '_blank');
 }

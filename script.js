cd72d65822202e5296dda1b70daf88d368daa6e3
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-01-09 18:04:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function openLink() {
+function openTwitch() {
   window.open('https://twitch.tv/NotLe0nard', '_blank');
+}
+function openODP() {
+  window.open('https://download1511.mediafire.com/vqysr2wt7wcgFJfibjFWRWXIlgVEkwswQyEaBtTIxR9gQLLSQPFUsF0xOZkyQLzpVg9TJmj8iD5D1LGaP1w7fya91trzcqAI8RrJC32-o7jsUwze_RF2CWemk-FGP6xbeTzlqUljUTTUZueSYIcIhwvaLXvva16mLO2V7sJOFhyl/isnmbwwc2urnl6n/WIDERSTANDSGRUPPE+%E2%80%9EDIE+WEI%C3%9FE+ROSE%E2%80%9D.odp', '_blank');
 }

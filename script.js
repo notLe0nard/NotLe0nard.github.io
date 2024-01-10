21562185df9eb1f10a68e2436138b945e1180fd9
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-01-10 22:26:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function openTwitch() {
   window.open('https://twitch.tv/NotLe0nard', '_blank');
 }
 function openODP() {
-  window.open('https://kappa.lol/nnF7T', '_blank');
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
 }

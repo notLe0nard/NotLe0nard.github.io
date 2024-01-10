6b74e948974a701e0fa124ba03322f81ad36f109
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-01-10 09:47:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function openTwitch() {
   window.open('https://twitch.tv/NotLe0nard', '_blank');
 }
 function openODP() {
-  window.open('https://download1523.mediafire.com/gexlyqbey8sgZVdpIiOxsi12AJSEl6mDe-MjMhTwnZNp1fctLUcDyrUReptRc_-CGEET8--Q7HJCn0gz9xKb9r_CSeX6Ap4gVuhVUZZArbZR6AOSgdYPRFymTcdo2weK4cbG3WVvUpT7wiyBMEO5XY7B4dnW28e2B-vymAePd44p/gifc4nujgzpgxfz/WIDERSTANDSGRUPPE+%E2%80%9EDIE+WEI%C3%9FE+ROSE%E2%80%9D+%282%29.odp', '_blank');
+  window.open('https://kappa.lol/nnF7T', '_blank');
 }

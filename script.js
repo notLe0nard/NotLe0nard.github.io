c86d20116d3e496f3902e7b9bca741f03b57f450
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-01-13 13:10:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function openTwitch() {
   window.open('https://twitch.tv/NotLe0nard', '_blank');
 }
-function openODP() {
-  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+function openIG() {
+  window.open('https://www.instagram.com/notle0nard', '_blank');
+}
+function openYT() {
+  window.open('https://www.youtube.com/channel/UCi96eLfDv4zsrwwYAIe1FXA', '_blank');
+}
+function openDC() {
+  alert("Discord: @NotLe0nard");
 }

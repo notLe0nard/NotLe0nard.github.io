eb3738e01470df18ce0a481156f03a79eaee48bc
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-01-14 18:43:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -26,9 +26,21 @@ async function startSite(){
   await sleep(100);
   document.getElementById("dc").style.fontSize = "6vmin";
 }
-startSite();
+
+function secret(){
+  kok++;
+  if(kok == 5){
+    window.open('https://kappa.lol/z9nML.odp', '_blank');
+  }
+  if(kok == 6){
+    window.open('https://kappa.lol/sReiX.pptx', '_blank');
+  }
+  
+}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+kok = 0;
+startSite();

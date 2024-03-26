94241d6b17eeb4e305e66eb3ebd6b689c143454a
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-03-27 00:44:04 +0100

    Add files via upload

---
+++ aufjott/script.js
@@ -0,0 +1,9 @@
+function reloadPage() {
+    location.reload();
+}
+  
+  // Set the interval in milliseconds (1000 milliseconds = 1 second)
+  const interval = 10 * 1000; // Change this value to your desired interval
+  
+  setInterval(reloadPage, interval);
+  

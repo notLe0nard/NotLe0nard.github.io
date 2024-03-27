84e3e84f7ac35b97bd3e0c2c66f081e1eaa279d5
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-03-27 22:37:26 +0100

    Update script.js

--- aufjott/script.js
+++ aufjott/script.js
@@ -3,7 +3,7 @@ function reloadPage() {
 }
   
   // Set the interval in milliseconds (1000 milliseconds = 1 second)
-  const interval = 10 * 1000; // Change this value to your desired interval
+  const interval = 10 * 30000; // Change this value to your desired interval
   
   setInterval(reloadPage, interval);
   

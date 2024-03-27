94ca6d0ef615ac2f20e482a8ca1c6a65f8bb9ff2
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-03-27 22:45:57 +0100

    Add files via upload

--- aufjott/script.js
+++ aufjott/script.js
@@ -1,9 +1,23 @@
 function reloadPage() {
     location.reload();
 }
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100000);
+}
+
+function Sleep(milliseconds) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+async function reloadImage1() {
+  document.getElementById('aufjott2').style.opacity = "1"
+  var newImageUrl = "https://www.gommehd.net/place/api/image/?lu=undefined?v=" + getRandomNumber();
+  document.getElementById('aufjott').style.backgroundImage = "url('" + newImageUrl + "')"
+  await Sleep(2000)
+  document.getElementById('aufjott2').style.opacity = "0"
   
-  // Set the interval in milliseconds (1000 milliseconds = 1 second)
-  const interval = 10 * 30000; // Change this value to your desired interval
-  
-  setInterval(reloadPage, interval);
-  
+}
+
+setInterval(reloadPage, 120000);
+setInterval(reloadImage1, 30000);

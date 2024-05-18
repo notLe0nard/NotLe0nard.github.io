211485fd883cc23e9543ac479cac5e62817a072f
notLe0nard <119421834+notLe0nard@users.noreply.github.com>
2024-05-18 22:49:31 +0200

    english FeelsDankMan

--- bedgebot/commands/script.js
+++ bedgebot/commands/script.js
@@ -1,184 +1,184 @@
 const commands = [
+  {
+    name: "#time",
+    description: "The current time.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#ping",
+    description: "Command to check if the bot is online.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#test",
+    description: "Just tests the bot or something.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#version",
+    description: "The current version of the bot.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#help | #commands",
+    description: "This website.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#following [username]",
+    description: "See who someone is following.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#vips",
+    description: "List of all VIPs in the channel", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#mods",
+    description: "List of all mods in the channel.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#founders",
+    description: "List of all founders in the channel.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#echo [message]",
+    description: "Repeats your message.", // Translated from German to English
+    category: "Everyone (Mod can add ? or ! at the beginning)",
+  },
+  {
+    name: "#color",
+    description: "Shows your chat color as HEX code.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#fill [Emote]",
+    description: "Fills the chat with Emotes.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#random",
+    description: "A random number from 0-100.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#afk [message]",
+    description: "You go afk. You can specify a message why you are gone or not. When you come back, the bot sends a message how long you were gone.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#gn [message]",
+    description: "You go to sleep. You can specify a message or not. When you come back, the bot sends a message how long you slept.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#isafk [User]",
+    description: "See if a specific chatter is AFK or asleep.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#spam [number] [message]",
+    description: "Sends your message multiple times.", // Translated from German to English
+    category: "Mod",
+  },
+  {
+    name: "#chatting [User] [Channel]",
+    description: "You see how many messages a user has written in a channel.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#cock",
+    description: "Your cock size.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#glitch",
+    description: "Glitches your message. (Only works in Chatterino.)", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#prefix",
+    description: "Changes the prefix of the bot. Default: #", // Translated from German to English
+    category: "Mod",
+  },
+  {
+    name: "#restart",
+    description: "Restarts the bot.", // Translated from German to English
+    category: "Owner",
+  },
+  {
+    name: "#stop",
+    description: "Stops the bot.", // Translated from German to English
+    category: "Owner",
+  },
+  {
+    name: "#channels",
+    description: "All channels the bot is active in.", // Translated from German to English
+    category: "Owner",
+  },
     {
-        name: "#time",
-        description: "Die aktuelle zeit.",
-        category: "Jeder",
-    },
-    {
-        name: "#ping",
-        description: "Command um zu schauen ob der Bot online ist.",
-        category: "Jeder",
-    },    
-    {
-        name: "#test",
-        description: "Joa testet halt den bot oderso.",
-        category: "Jeder",
-    },   
-    {
-        name: "#version",
-        description: "Die aktuelle Version vom Bot.",
-        category: "Jeder",
-    }, 
-    {
-        name: "#help | #commands",
-        description: "Diese Website.",
-        category: "Jeder",
-    },    
-    {
-        name: "#following [username]",
-        description: "Schaue nach wem jemand folgt.",
-        category: "Jeder",
-    },    
-    {
-        name: "#vips",
-        description: "Liste von allen VIPs im channel",
-        category: "Jeder",
-    },    
-    {
-        name: "#mods",
-        description: "Liste von allen Mods im channel.",
-        category: "Jeder",
-    },    
-    {
-        name: "#founders",
-        description: "Liste von allen foundern im channel.",
-        category: "Jeder",
-    },    
-    {
-        name: "#echo [Nachricht]",
-        description: "Wiederhohlt deine nachricht.",
-        category: "Jeder (Mod kann z.B. ? oder ! am anfang hinzufügen)",
-    },    
-    {
-        name: "#color",
-        description: "Zeigt deine Chat-Farbe als HEX-Code.",
-        category: "Jeder",
-    },    
-    {
-        name: "#fill [Emote]",
-        description: "Füllt den chat mit Emotes.",
-        category: "Jeder",
-    },    
-    {
-        name: "#random",
-        description: "Eine zufällige zahl von 0-100.",
-        category: "Jeder",
-    },    
-    {
-        name: "#afk [Nachricht]",
-        description: "Du gehst afk. Du kannst eine Nachricht angeben wieso du weg gehst oder auch nicht. Wenn du zurück kommst sendet der bot eine nachricht wie lange du weg warst.",
-        category: "Jeder",
-    },    
-    {
-        name: "#gn [Nachricht]",
-        description: "Du gehst schlafen. Du kannst eine Nachricht angeben oder auch nicht. Wenn du zurück kommst sendet der bot eine nachricht wie lange du geschlafen hast.",
-        category: "Jeder",
-    },    
-    {
-        name: "#isafk [User]",
-        description: "Schaue nach ob ein bestimmter Chatter gerade AFK oder am schlafen ist.",
-        category: "Jeder",
-    },    
-    {
-        name: "#spam [Anzahl] [Nachricht]",
-        description: "Sendet deine Nachricht vermehrt.",
-        category: "Mod",
-    },    
-    {
-        name: "#chatting [User] [Kanal]",
-        description: "Du siehst, wie viele Nachrichten ein user in einem Kanal geschrieben hat.",
-        category: "Jeder",
-    },
-    {
-        name: "#kok",
-        description: "Deine kok größe.",
-        category: "Jeder",
-    },
-    {
-        name: "#glitch",
-        description: "Glitcht deine nachricht. (Geht nur in Chatterino.)",
-        category: "Jeder",
-    },
-    {
-        name: "#prefix",
-        description: "Ändert den Prefix des Bots. Default: #",
-        category: "Mod",
-    },
-    {
-        name: "#restart",
-        description: "Startet den bot neu.",
-        category: "Owner",
-    },
-    {
-        name: "#stop",
-        description: "Stopt den bot.",
-        category: "Owner",
-    },
-    {
-        name: "#channels",
-        description: "Alle channel in dem der Bot aktiv ist.",
-        category: "Owner",
-    },
-    {
-        name: "#average",
-        description: "Die Average Zuschauer des kanals in den lezten 30 Tagen.",
-        category: "Jeder",
-    },
-    {
-        name: "#streamage",
-        description: "Wie lange der Kanal schon gestreamt hat.",
-        category: "Jeder",
-    },
-    {
-        name: "#maxviewers",
-        description: "Der Viewerrekord des Kanals.",
-        category: "Jeder",
-    },
-    {
-        name: "#followers",
-        description: "Wie viele Follower der Kanal hat.",
-        category: "Jeder",
-    },
-    {
-        name: "#streams | #streak",
-        description: "twitchtracker.com/[kanal]/streams",
-        category: "Jeder",
-    },
-    {
-        name: "#bedgis",
-        description: "Dir wird eine zufällige Anzahl zwischen 0 und 1000 Bedgi's zugeschrieben. Danach musst du eine Stunde warten, bis du wieder den Command benutzen kannst.",
-        category: "Jeder",
-    },
-    {
-        name: "#gamba [Anzahl / all]",
-        description: "Glücksspiel!",
-        category: "Jeder",
-    },
-    {
-        name: "#math [Aufgabe]",
-        description: "Berechnet eine Mathe Aufgabe",
-        category: "Jeder",
-    },
-    {
-        name: "#chatis",
-        description: "Generiert ein Chat overlay",
-        category: "",
-    },
-    {
-        name: "#addchannel",
-        description: "Fügt den bot in einem Kanal hinzu.",
-        category: "Owner",
-    },
-    {
-        name: "#rainbow",
-        description: "🌈",
-        category: "Mod",
-    },
-    {
-        name: "#user [User]",
-        description: "Informationen über einen User.",
-        category: "Jeder",
-    },
+    name: "#average",
+    description: "The average viewers of the channel in the last 30 days.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#streamage",
+    description: "How long the channel has been streaming.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#maxviewers",
+    description: "The viewer record of the channel.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#followers",
+    description: "How many followers the channel has.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#streams | #streak",
+    description: "twitchtracker.com/[channel]/streams", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#bedgis",
+    description: "You will be assigned a random number between 0 and 1000 Bedgi's. Then you have to wait an hour before you can use the command again.", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#gamba [number / all]",
+    description: "Gambling!", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#math [task]",
+    description: "Solves a math problem", // Translated from German to English
+    category: "Everyone",
+  },
+  {
+    name: "#chatis",
+    description: "Generates a chat overlay", // Translated from German to English
+    category: "",
+  },
+  {
+    name: "#addchannel",
+    description: "Adds the bot to a channel.", // Translated from German to English
+    category: "Owner",
+  },
+  {
+    name: "#rainbow",
+    description: "", // Translated from German to English
+    category: "Mod",
+  },
+  {
+    name: "#user [User]",
+    description: "Information about a user.", // Translated from German to English
+    category: "Everyone",
+  },
 ];
 
 
